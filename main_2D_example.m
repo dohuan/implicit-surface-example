a7f4b94a7db574657d41de8fc60bf4993e664cf5
dohuan <dohuan@msu.edu>
2015-07-16 15:13:01 -0400

    071615 3:12pm

--- main_2D_example.m
+++ main_2D_example.m
@@ -16,15 +16,53 @@ alpha = (0:0.5:2*pi)';
 r = 0.5;
 [inner.x,inner.y] = pol2cart(alpha,r);
 
-hold on
-plot(on_sur.x,on_sur.y,'bo','MarkerSize',10);
-plot(inner.x,inner.y,'bx','MarkerSize',10);
-plot(outer.x,outer.y,'bd','MarkerSize',10);
-hold off
-
 X = [on_sur.x on_sur.y];
 Y = zeros(size(X,1),1);
-X = [X;[inner.x inner.y]];
-Y = [Y;-1*ones(size(inner.x,1),1)];
-X = [X;[outer.x outer.y]];
-Y = [Y;-1*ones(size(inner.x,1),1)];
+% X = [X;[inner.x inner.y]];
+% Y = [Y;-1*ones(size(inner.x,1),1)];
+% X = [X;[outer.x outer.y]];
+% Y = [Y;1*ones(size(inner.x,1),1)];
+
+max_x = max(X(:,1));
+max_y = max(X(:,2));
+min_x = min(X(:,1));
+min_y = min(X(:,2));
+
+gridsize = 50;
+x_mesh = linspace(max_x,min_x,gridsize);
+y_mesh = linspace(max_y,min_y,gridsize);
+[S1,S2] = meshgrid(x_mesh,y_mesh); % [middle shortest longest]
+S_temp = [S1(:),S2(:)];
+
+covfunc  = @covSEard;
+likfunc  = @likGauss;
+meanfunc = @meanOne;
+
+hyp.cov(1) = log(0.5); % bandwidth of x
+hyp.cov(2) = log(0.5); % bandwidth of y
+hyp.cov(3) = log(1);   % sig_f
+
+hyp.lik = log(0.03);
+
+[est, ~] = gp(hyp, @infExact, meanfunc, covfunc, likfunc, X, Y, S_temp);
+
+field = reshape(est,gridsize,gridsize);
+
+figure(1)
+surf(S1,S2,field,'EdgeColor','none');
+hold on
+plot3(on_sur.x,on_sur.y,zeros(size(on_sur.x,1),1),'k','LineWidth',2.5);
+set(gca,'FontSize',16);
+hold off
+
+figure(2)
+pcolor(S1,S2,field);
+colorbar
+hold on
+plot(on_sur.x,on_sur.y,'ko','MarkerSize',10,'LineWidth',2.5);
+plot(inner.x,inner.y,'kx','MarkerSize',10,'LineWidth',2.5);
+plot(outer.x,outer.y,'kd','MarkerSize',10,'LineWidth',2.5);
+hold off
+set(gca,'FontSize',16);
+
+
